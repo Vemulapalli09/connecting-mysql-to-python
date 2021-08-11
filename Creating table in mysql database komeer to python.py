b522b0f9bbe0c1ee63d93cb5dcb5439a9784debe
Vemulapalli09 <67271452+Vemulapalli09@users.noreply.github.com>
2021-08-11 12:00:48 +0100

    Add files via upload

---
+++ Creating table in mysql database komeer to python.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(host="localhost",user="root",passwd="Ajay@1234",database= "komeer")
+
+mycursor = mydb.cursor()
+
+mycursor.excute("CREATE TABLE person (name varchar(50), age INTEGER(10), personID INTEGER AUTO_INCREMENT PRIMARY KEY)")
+mycursor.excute("DESCRIBE Person")
+
+for x in mycursor:
+	print(x)
+	
+
+
